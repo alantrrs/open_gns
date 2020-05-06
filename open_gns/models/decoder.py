4661c4bec804566e83618dda0523b9294f20303e
ambidextrvs <alan.trrs.ngls@gmail.com>
2020-05-06 00:30:08 -0700

    Fix decoder output

--- open_gns/models/decoder.py
+++ open_gns/models/decoder.py
@@ -5,7 +5,7 @@ from open_gns.models.encoder import make_mlp
 class Decoder(torch.nn.Module):
     def __init__(self, input_size):
         super(Decoder, self).__init__()
-        self.decoder = make_mlp(input_size, output_size=1, layer_norm=False)
+        self.decoder = make_mlp(input_size, output_size=3, layer_norm=False)
     
     def forward(self, x):
         return self.decoder(x)
